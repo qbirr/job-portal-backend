c7476d40111baab69ee9917ea282fb3ef311f26d
hherdianto <hherdianto@gmail.com>
2023-09-23 18:37:20 +0700

    add linked in api

--- public/assets/js/web/js/news_letter/news_letter.js
+++ public/assets/js/web/js/news_letter/news_letter.js
@@ -8,11 +8,13 @@ listenSubmit('#newsLetterForm', function (event) {
   var email = $('#mc-email').val();
   var emailExp = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
   var emailCheck = email == '' ? true : emailExp.test(email) ? true : false;
+
   if (!emailCheck) {
     displayErrorMessage('Please enter a valid Email');
     return false;
-  }
-  // loadingButton.button('loading');
+  } // loadingButton.button('loading');
+
+
   processingBtn('#newsLetterForm', '#btnLetterSave', 'loading');
   $.ajaxSetup({
     headers: {
@@ -32,8 +34,8 @@ listenSubmit('#newsLetterForm', function (event) {
       displayErrorMessage(result.responseJSON.message);
     },
     complete: function complete() {
-      $('#mc-email').val('');
-      // loadingButton.button('reset');
+      $('#mc-email').val(''); // loadingButton.button('reset');
+
       processingBtn('#newsLetterForm', '#btnLetterSave');
     }
   });
